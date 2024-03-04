8b6f5e1f727c190d6ae06af2d9c637a30842fa1d
Natapoloat <natapoloat@gmail.com>
2024-03-04 10:51:36 +0700

    prime1

--- api.py
+++ api.py
@@ -14,5 +14,14 @@ def hello():
 def plus(num1,num2):
     return {"result":(float(num1)+float(num2))}
 
+@app.route("/is_prime/<int: x>")
+def prime(x):
+    if x <= 1:
+        return {'result' : False}
+    for i in range(2, int(x**0.5) + 1):
+        if x % i == 0:
+            return {'result' : False}
+    return {'result' : True}
+
 if __name__ == '__main__':
       app.run(port=5000)
